114f4fab24d55d0d5c4014d6734570dd4576513e
Tony Huang <tonyhchii@gmail.com>
2024-08-29 14:16:11 -0400

    added dialog box form, which can add new books

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const myLibrary = [
     {
         title: 'Tony\'s book',
-        author: 'Tony'
+        author: 'Tony',
+        pages: 1
     },
     {
         title: 'Game Of Thrones',
-        author: 'George R R Martin'
+        author: 'George R R Martin',
+        pages: 333
     }
 
 ];
 
-function Book(title, author) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
+    this.pages = pages
 }
 
 function addBookToLibrary(book) {
@@ -21,11 +24,40 @@ function addBookToLibrary(book) {
 
 function displayBooks() {
     const container = document.querySelector('.container');
+    container.innerHTML = "";
     myLibrary.forEach((book,index) => {
         const bookCard = document.createElement('div');
         bookCard.textContent = book.title;
-        container.append(bookCard)
+        container.append(bookCard);
     });
 }
+
+const newBookForm = document.querySelector('.newBookForm');
+const showButton = document.querySelector('.show');
+const closeButton = document.querySelector('.close');
+
+showButton.addEventListener('click', () => {
+    newBookForm.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    newBookForm.close();
+});
+
+document.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const dialogForm = document.querySelector('dialog form');
+    const formData = new FormData(dialogForm)
+
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const pages = formData.get('pages');
+
+    const newBook = new Book(title, author, pages);
+    addBookToLibrary(newBook);
+    displayBooks();
+    dialogForm.reset();
+    newBookForm.close();
+});
 
 displayBooks();
